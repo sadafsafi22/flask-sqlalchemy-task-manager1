67d7280d1363ea0bd88f2818cc11ea7e7a30cf31
sadafsafi22 <safisadaf96@gmail.com>
2023-03-26 10:41:40 +0000

    database created with simple setup

---
+++ taskmanager/routes.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from taskmanager import app, db
+import os
+from taskmanager.models import Category, Task
+
+@app.route("/")
+def home():
+    return render_template("base.html")
